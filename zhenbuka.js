00c63ef4304fdb4e0e859d5242bfb2e695a23dbe
VINCENTCHEN94 <35206112+VINCENTCHEN94@users.noreply.github.com>
2022-11-18 17:20:18 +0800

    Update zhenbuka.js

--- zhenbuka.js
+++ zhenbuka.js
@@ -1,2 +1,2 @@
-var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/zhenbuka.css" type="text/css">').replace(/'6572'/g, "'6578'");
+var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/VINCENTCHEN94/qx/main/zhenbuka.css" type="text/css">').replace(/'6572'/g, "'6578'");
 $done({ body });
