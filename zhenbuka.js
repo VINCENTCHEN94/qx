02449892c0fb96840c3abb5414d3139c68d3e39c
VINCENTCHEN94 <35206112+VINCENTCHEN94@users.noreply.github.com>
2022-11-22 10:04:41 +0800

    Delete zhenbuka.js

--- zhenbuka.js
+++
@@ -1,2 +0,0 @@
-var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/VINCENTCHEN94/qx/main/zhenbuka.css" type="text/css">').replace(/'6572'/g, "'6578'");
-$done({ body });
